78bd0c5c1961bb60383cbb1d015d73647f095b78
markmikh <mark.mikhail@outlook.com>
2024-01-23 12:19:07 -0500

    adding spacer commponent still in development not completed

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,21 @@
+import react from "react";
+import { View } from "react-native";
+import styled from "styled-components/native"
+
+const TopSmall = styled.View'
+    marginTop: 4px;
+';
+
+const TopMedium = styled.View'
+    marginTop: 8px;
+';
+const TopLarge = styled.View'
+    marginTop: 16px;
+';
+
+export const Spacer = ({ varient }) => {
+    switch(variant) {
+
+
+    }
+}
